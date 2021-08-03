214ece19525ae424423a03beea3dbd20725968d0
Prayag <35863260+Prayag22@users.noreply.github.com>
2021-08-03 14:48:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request
+
+import marks as m
+
+app =Flask(__name__)
+
+@app.route("/", methods = ["POST"]) 
+def marks(): 
+    if request.method == "POST":
+        hrs=request.form["hrs"]
+        marks_pred=m.marks_prediction(hrs)
+        mks=marks_pred
+        
+        return render_template("index.html",my_marks = mks)
+
+@app.route("/")
+def submit():
+    return render_template("index.html")
+    
+if __name__ == "__main__": 
+     app.run(debug=True)
